2d099144d43f560ebd7c519dd237d42902b084ed
MHosein Forghani <mohofo7@gmail.com>
2021-04-10 17:38:19 +0430

    feat: add time to messages

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -4,7 +4,14 @@ import "./style.scss";
 
 const Message: React.FC<IMessage> = (props) => {
   const { content, date, self } = props;
-  return <div className={`message ${self ? "message_self" : ""}`}>{content}</div>;
+  return (
+    <div className={`message__wrapper ${self ? "message__wrapper_self" : ""}`}>
+      <div className={`message ${self ? "message_self" : ""}`}>{content}</div>
+      <div className="message__date">{`${new Date(date).getHours()}:${new Date(
+        date
+      ).getMinutes()}`}</div>
+    </div>
+  );
 };
 
 export default Message;
